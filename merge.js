8c087c657aa0e0e0bf46be1c09d84ff359df4f05
Nick <nsand3@gmail.com>
2021-09-26 15:16:18 -0700

    completed merge function

--- merge.js
+++ merge.js
@@ -1,5 +1,28 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let arr = [];
+  let idx1 = 0;
+  let idx2 = 0;
+
+  while (arr1.length > idx1 && arr2.length > idx2) {
+    if (arr1[idx1] < arr2[idx2]) {
+      arr.push(arr1[idx1]);
+      idx1++;
+    } else {
+      arr.push(arr2[idx2]);
+      idx2++;
+    }
+  }
+  while (arr1.length > idx1) {
+    arr.push(arr1[idx1]);
+    idx1++;
+  }
+  while (arr2.length > idx2) {
+    arr.push(arr2[idx2]);
+    idx2++;
+  }
+  return arr;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
